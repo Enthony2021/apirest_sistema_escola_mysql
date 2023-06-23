a31c7848268a000cf3702f2c87923054e4b2f6da
Enthony Stevie <enthonyoliveira96@gmail.com>
2023-06-23 09:56:15 -0700

    cors adicionado

--- app.js
+++ app.js
@@ -6,20 +6,39 @@ dotenv.config();
 import "./src/database";
 
 import express from "express";
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from "./src/routes/homeRoutes";
 import userRoutes from "./src/routes/userRoutes";
 import tokenRoutes from "./src/routes/tokenRoutes";
 import alunoRoutes from "./src/routes/alunoRoutes";
 import fotoRoutes from "./src/routes/fotoRoutes";
 
+const whiteList = [
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 class App {
   constructor() {
-    this.app = express();
+    this.app = express(corsOptions);
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, "uploads")));
